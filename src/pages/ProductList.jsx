1a22a3f817b55eef4d68ba83e8385c266f43526b
ImrenR <imrenrahbay@gmail.com>
2025-10-14 22:48:24 +0100

    error State

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,28 +4,39 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
 
   const getData = async () => {
-    const { data } = await axios(BASE_URL);
-    setProducts(data);
+    try {
+      const { data } = await axios(BASE_URL);
+      setProducts(data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+    }
   };
 
   useEffect(() => {
     getData();
   }, []);
 
+  if(error === true){
+    return <p>Something went wrong..</p>
+  }
+
   return (
     <div className="container mt-3">
       <div className="d-sm-block d-md-flex">
-        {"" ? (
+        {loading ? (
           <p>Loading.....</p>
         ) : (
           <>
             <article id="product-panel" className="col-md-6">
               {products.map((urun) => (
-                <ProductCard urun={urun} getData={getData}/>
+                <ProductCard urun={urun} getData={getData} />
               ))}
             </article>
             <article>
